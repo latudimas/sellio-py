bde2891d25e9816243a29d24f421eda9ab4cfdc8
latudimas <riswandha.ld@gmail.com>
2024-08-31 15:24:31 +0700

    init project

---
+++ main.py
@@ -0,0 +1,17 @@
+import fasthtml.common as fh
+
+app, rt = fh.fast_app()
+
+
+@rt("/")
+def getIndex():
+    return fh.Div(fh.P("Hello World"), hx_get="/change")
+
+
+@rt("/change")
+def getChange():
+    return fh.P("Nice to be here!")
+
+
+if __name__ == "__main__":
+    fh.serve()
